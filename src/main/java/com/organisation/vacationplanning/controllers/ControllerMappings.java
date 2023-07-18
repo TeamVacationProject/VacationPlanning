f0961d12f14067d50bc3a073b83b73f1b85afccf
Dmitry Kopyshov <dkopysh@gmail.com>
2023-07-18 19:50:38 +0300

    Testing remote access

--- src/main/java/com/organisation/vacationplanning/controllers/ControllerMappings.java
+++ src/main/java/com/organisation/vacationplanning/controllers/ControllerMappings.java
@@ -3,5 +3,6 @@ package com.organisation.vacationplanning.controllers;
 public class ControllerMappings {
     /*
     Класс для перенаправления поступающих от клиента запросов на соотвествующие сервисы
+    Вносим изменения
     */
 }
