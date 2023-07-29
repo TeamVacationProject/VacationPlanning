ed5ea18acc39e3f2b7bdad9d80b3dc92aa8ead88
Dmitry Kopyshov <dkopysh@gmail.com>
2023-07-29 22:19:23 +0300

    Configure Database: add SQLite

---
+++ src/main/java/com/organisation/vacationplanning/database/entities/Employee.java
@@ -0,0 +1,52 @@
+package com.organisation.vacationplanning.database.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "SURNAME")
+    private String surname;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "PATRONYMIC")
+    private String patronymic;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+}
---
+++ src/main/java/com/organisation/vacationplanning/testingdata/TestData.java
@@ -0,0 +1,21 @@
+package com.organisation.vacationplanning.testingdata;
+
+import com.organisation.vacationplanning.database.HibernateUtil;
+import com.organisation.vacationplanning.database.entities.Employee;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class TestData {
+    public void addTestingEmployee(){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Employee employee = new Employee();
+            employee.setSurname("Petrov");
+            employee.setFirstName("Petr");
+            employee.setPatronymic("Petrovich");
+
+            Transaction tx = session.beginTransaction();
+            session.persist(employee);
+            tx.commit();
+        }
+    }
+}
