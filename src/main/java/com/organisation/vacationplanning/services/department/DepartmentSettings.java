0c8a9441f3598bb7c3eb126b07c98b3b5e338f9f
Dmitry Kopyshov <dkopysh@gmail.com>
2023-07-30 10:54:20 +0300

    Add DepartmentSettings.java with including Employees

--- src/main/java/com/organisation/vacationplanning/services/department/DepartmentSettings.java
+++ src/main/java/com/organisation/vacationplanning/services/department/DepartmentSettings.java
@@ -1,5 +1,78 @@
 package com.organisation.vacationplanning.services.department;
 
-public class DepartmentSettings {
+import com.organisation.vacationplanning.database.HibernateUtil;
+import com.organisation.vacationplanning.database.entities.Employee;
+import com.organisation.vacationplanning.services.IVacationController;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.IWebExchange;
+
+import java.io.Writer;
+import java.util.List;
+
+public class DepartmentSettings implements IVacationController {
+
     //Управление настройками департамента/организации/подразделения
+
+    private static final String ALL_EMPLOYEES = "from Employee";
+    private WebContext ctx;
+    @Override
+    public void process(IWebExchange webExchange, ITemplateEngine templateEngine, Writer writer) throws Exception {
+        ctx = new WebContext(webExchange, webExchange.getLocale());
+        if(webExchange.getRequest().getMethod().equals("POST")) {
+            handlePost(webExchange);
+            handleGet(templateEngine, writer);
+        }
+        if (webExchange.getRequest().getMethod().equals("GET")) {
+            handleGet(templateEngine, writer);
+        }
+    }
+
+    private void handlePost(IWebExchange webExchange) {
+        String id;
+        String surname;
+        String firstName;
+        String patronymic;
+        if (webExchange.getRequest().containsParameter("employee-id")) {
+
+/*            surname = webExchange.getRequest().getParameterValue("surname");
+            firstName = webExchange.getRequest().getParameterValue("first-name");
+            patronymic = webExchange.getRequest().getParameterValue("patronymic");*/
+
+            try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+                Transaction tx = session.beginTransaction();
+
+                tx.commit();
+            }
+        } else {
+/*            surname = webExchange.getRequest().getParameterValue("surname");
+            firstName = webExchange.getRequest().getParameterValue("first-name");
+            patronymic = webExchange.getRequest().getParameterValue("patronymic");
+            Employee employee = new Employee();
+            employee.setSurname(surname);
+            employee.setFirstName(firstName);
+            employee.setPatronymic(patronymic);*/
+            try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+                Transaction tx = session.beginTransaction();
+
+                tx.commit();
+            }
+        }
+    }
+
+    private void handleGet(ITemplateEngine templateEngine, Writer writer) {
+        List<Employee> employees;
+
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            Query<Employee> query = session.createQuery(ALL_EMPLOYEES, Employee.class);
+            employees = query.getResultList();
+            tx.commit();
+        }
+        ctx.setVariable("employees", employees);
+        templateEngine.process("settings", ctx, writer);
+    }
 }
